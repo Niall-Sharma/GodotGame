20574983d74e13f464f739813567086778ffee8d
Niall Sharma <Niall.k.sharma@gmail.com>
2023-09-15 17:22:09 -0500

    AudioStuff

--- MainMenu/MainMenu.gd
+++ MainMenu/MainMenu.gd
@@ -7,9 +7,9 @@ func _ready():
 	$Main.visible = true
 	$MenuTheme.play()
 
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
 
 func _on_options_button_pressed():
 	$Options.visible = true
@@ -30,3 +30,33 @@ func _on_start_button_pressed():
 func _on_back_button_pressed():
 	$Options.visible = false
 	$Main.visible = true # Replace with function body.
+
+
+func _on_master_volume_value_changed(value):
+	AudioServer.set_bus_layout(load("res://default_bus_layout.tres"))
+	AudioServer.set_bus_volume_db(2,value)
+	if value == 0:
+		AudioServer.set_bus_mute(2, true)
+	else :
+		AudioServer.set_bus_mute(2,false)
+
+
+
+func _on_sound_effects_volume_value_changed(value):
+	AudioServer.set_bus_layout(load("res://default_bus_layout.tres"))
+	AudioServer.set_bus_volume_db(1,value)
+	if value == 0:
+		AudioServer.set_bus_mute(1, true)
+	else :
+		AudioServer.set_bus_mute(1,false)
+
+
+
+func _on_music_volume_value_changed(value):
+	AudioServer.set_bus_layout(load("res://default_bus_layout.tres"))
+	AudioServer.set_bus_volume_db(0,value)
+	if value == 0:
+		AudioServer.set_bus_mute(0, true)
+	else :
+		AudioServer.set_bus_mute(0,false)
+
