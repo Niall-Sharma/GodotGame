f62a722eb405fedec17852484ea2cf9a28d72abf
Spork06 <spenserpraska@gmail.com>
2023-09-21 09:52:23 -0500

    w

--- Master/master.gd
+++ Master/master.gd
@@ -5,6 +5,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
 	$"GUI/Pause Menu".hide()
 	$"GUI/PauseMenuBackground".hide()
 	$MainTheme.play()
