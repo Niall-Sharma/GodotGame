7edf039ac9cb2d602b44103de1e234499b92db75
AsterSailor <artanddna@gmail.com>
2023-09-21 10:23:17 -0500

    r

--- Randomi/randomi.gd
+++ Randomi/randomi.gd
@@ -1,9 +1,12 @@
 extends Node2D
-
+@export var land_scene: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var scene = load("res://land.tscn")
+	var land = scene.instantiate()
+	land.initialize(500,500)
+	add_child(land)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
