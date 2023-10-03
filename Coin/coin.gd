be34b14dd21b3b65eda1bc7d837d74038450f83b
PerpetualObligation <jl46692@wdmcs.org>
2023-10-03 13:35:39 -0500

    YESS

--- Coin/coin.gd
+++ Coin/coin.gd
@@ -13,8 +13,6 @@ func _process(delta):
 func _leave():
 	$coin_sprite.visible = false
 	for child in get_children():
-		if(child.get_child(0) != null):
-			child.get_child(0).queue_free()
 		child.queue_free()
 		print(child)
 	queue_free()
