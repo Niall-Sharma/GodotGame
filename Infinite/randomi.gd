920c31c3d386f1ec7c6980685247a313fc003147
AsterSailor <artanddna@gmail.com>
2023-10-05 12:58:50 -0500

    cc

--- Infinite/randomi.gd
+++ Infinite/randomi.gd
@@ -1,7 +1,7 @@
 extends Node2D
 @export var land: PackedScene
 var scene = preload("res://Infinite/land.tscn")
-var scener = preload("res://Infinite/randi.tscn")
+var scener = preload("res://Infinite/clarice.tscn")
 var landi = scene.instantiate()
 var rx = landi.position.x
 var ry = landi.position.y
