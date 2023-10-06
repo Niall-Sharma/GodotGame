340f16e0264e1b776a221c278899606b5ae827ca
PerpetualObligation <jl46692@wdmcs.org>
2023-10-06 13:33:27 -0500

    thing

--- Coin_Display/coin_display.gd
+++ Coin_Display/coin_display.gd
@@ -4,9 +4,10 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	$RichTextLabel.text = 0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if($RichTextLabel.text != $/root/Master._get_coin_counter()):
+		$RichTextLabel.text = $/root/Master._get_coin_counter()
