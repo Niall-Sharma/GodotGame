689f0d47460c7b29a0edb072f4ed2bc5fd60f590
Niall Sharma <Niall.k.sharma@gmail.com>
2023-10-06 13:20:19 -0500

    nvm

--- Player/player.gd
+++ Player/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 var hasLanded = false
+var canDash = true
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var health = 100
@@ -32,8 +33,7 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		$JumpSound.play()
-		
-
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
