ec882396e3d3b28e204161889e367b2c5d67bd38
Niall Sharma <Niall.k.sharma@gmail.com>
2023-10-06 13:08:29 -0500

    restart button added

--- Master/master.gd
+++ Master/master.gd
@@ -53,3 +53,9 @@ func _on_back_button_pressed():
 func _on_menu_button_pressed():
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://MainMenu/MainMenu.tscn")
+
+
+func _on_restart_button_pressed():
+	get_tree().reload_current_scene()
+	get_tree().paused = false
+	_change_level(Globalvars.getLevel())
