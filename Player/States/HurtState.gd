d537751db65a30cbcb716050e4fc072bbc1224f9
Niall Sharma <Niall.k.sharma@gmail.com>
2023-10-10 13:14:48 -0500

    Player knockback

--- Player/States/HurtState.gd
+++ Player/States/HurtState.gd
@@ -4,6 +4,9 @@ class_name HurtState
 
 @export var stateDuration : float = 0.5
 @export var hurtSound : AudioStreamPlayer
+@export var knockback_amount : Vector2
+@export var playerSprite : Sprite2D
+
 var animationPlayback : AnimationNodeStateMachinePlayback
 func onEnter():
 	hurtSound.play()
@@ -11,6 +14,13 @@ func onEnter():
 	$HurtTimer.start()
 	animationPlayback = animationTree["parameters/playback"]
 	animationPlayback.travel("Hurt")
+	if playerSprite.flip_h == true:
+		character.velocity+= knockback_amount
+	else:
+		character.velocity.x += -knockback_amount.x
+		character.velocity.y += knockback_amount.y
+	character.move_and_slide()
+	
 	
 	
 func onExit():
