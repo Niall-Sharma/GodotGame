16d1741131db85926b0d1d5a387571736d74167b
Spork06 <spenserpraska@gmail.com>
2023-10-10 13:41:49 -0500

    Update player.gd

--- Player/player.gd
+++ Player/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var health = 100
+var level1complete : bool = true
 
 @onready var animationTree = $AnimationTree
 @onready var heatlhBar = $PlayerGUI/HealthBar
