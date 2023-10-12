92069824f792ec1448267e6b8a854aa4a0db9a18
PerpetualObligation <jl46692@wdmcs.org>
2023-10-12 11:58:40 -0500

    Update coin_display.gd

--- Coin_Display/coin_display.gd
+++ Coin_Display/coin_display.gd
@@ -1,13 +1,15 @@
 extends Node2D
-
+var string_version
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$RichTextLabel.text = "0"
+	string_version = "0"
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if($RichTextLabel.text != $/root/Master._get_coin_counter()):
-		$RichTextLabel.text = $/root/Master._get_coin_counter()
+	string_version = str($/root/Master._get_coin_counter())
+	if($RichTextLabel.text != string_version):
+		$RichTextLabel.text = string_version
