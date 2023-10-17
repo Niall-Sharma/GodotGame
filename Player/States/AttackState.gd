7bce80e09b3a773a5dd86704bf0c69b14e0081c2
Niall Sharma <Niall.k.sharma@gmail.com>
2023-10-17 13:04:05 -0500

    enemy takes damage and gets knocked back

--- Player/States/AttackState.gd
+++ Player/States/AttackState.gd
@@ -4,11 +4,20 @@ class_name PlayerAttackState
 
 @export var attackTime : float = .3
 @export var attackTimer : Timer
+@export var knockback_amount : Vector2
+@export var playerSprite : Sprite2D
 
 func onEnter():
 	animationState.travel("Attack")
 	attackTimer.wait_time = attackTime
 	attackTimer.start()
+	if playerSprite.flip_h == true:
+		character.velocity+= knockback_amount
+	else:
+		character.velocity.x += -knockback_amount.x
+		character.velocity.y += knockback_amount.y
+	character.move_and_slide()
+	
 
 func onExit():
 	attackTimer.stop()
