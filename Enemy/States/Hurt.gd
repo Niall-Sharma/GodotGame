2f128e09fc2a21658655ed18e905ffb01ba6a75f
Niall Sharma <Niall.k.sharma@gmail.com>
2023-10-17 12:27:09 -0500

    Fix Enemy

--- Enemy/States/Hurt.gd
+++ Enemy/States/Hurt.gd
@@ -3,13 +3,14 @@ extends State
 class_name EnemyHurt
 
 @onready var hurtTimer : Timer = $HurtTimer
-
+@export var STANDBY_STATE : State
 
 func _on_hurt_timer_timeout():
-	animationState.travel("Hurt")
+	nextState = STANDBY_STATE
 
 func onEnter():
 	hurtTimer.start()
+	animationState.travel("Hurt")
 	
 func onExit():
 	hurtTimer.stop()
--- Player/States/GroundState.gd
+++ Player/States/GroundState.gd
@@ -11,6 +11,8 @@ func state_input(_event : InputEvent):
 		character.velocity.y = JUMP_VELOCITY
 		nextState = get_parent().states[0]
 		jumpSoundEffect.play()
+	if(_event.is_action_pressed("attack")):
+		nextState = get_parent().states[3]
 
 func onEnter():
 	pass
