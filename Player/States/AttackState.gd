8e8d8a10b577476abd6b106e2ca52955a445d872
Niall Sharma <Niall.k.sharma@gmail.com>
2023-10-17 12:47:09 -0500

    fixed EVERYTHING

--- Player/States/AttackState.gd
+++ Player/States/AttackState.gd
@@ -2,13 +2,18 @@ extends State
 
 class_name PlayerAttackState
 
+@export var attackTime : float = .3
+@export var attackTimer : Timer
+
 func onEnter():
 	animationState.travel("Attack")
+	attackTimer.wait_time = attackTime
+	attackTimer.start()
 
 func onExit():
-	pass
+	attackTimer.stop()
+	animationState.travel("Start")
 
 
-func _on_animation_player_animation_finished(anim_name):
-	if anim_name == "Attack":
-		nextState = get_parent().states[1]
+func _on_attack_timer_timeout():
+	nextState = get_parent().states[1]
