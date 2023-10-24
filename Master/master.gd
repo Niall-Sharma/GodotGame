80b5c3b186b779bc6ff6e8517b738070e79f959e
Niall Sharma <Niall.k.sharma@gmail.com>
2023-10-24 09:49:50 -0500

    Revert "Revert "Merge branch 'main' of https://github.com/Niall-Sharma/GodotGame""
    This reverts commit 4e5136af88ce0eca0877f1973760d4d8c013c4ca.

--- Master/master.gd
+++ Master/master.gd
@@ -4,7 +4,6 @@ var levels = [preload("res://Level1/level_1.tscn"),null,null,null,null,null,null
 var infiniteLevel = preload("res://Infinite/ran.tscn")
 var coinCounter
 
-#Takes a float as a parameter. Uses x as an index and selects level from levels array. Adds level as child of master. 
 func _change_level(x):
 	var level = levels[x].instantiate()
 	add_child(level)
@@ -22,11 +21,7 @@ func _ready():
 	AudioServer.set_bus_volume_db(0,-20)
 	_change_level(Globalvars.getLevel())
 	coinCounter = 0
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
 
-#When pause button is pressed 
 func _on_pause_button_pressed():
 	get_tree().paused = true
 	$"GUI/Pause Menu".show()
@@ -34,7 +29,7 @@ func _on_pause_button_pressed():
 	$GUI/PauseMenuBackground.show()
 
 
-#Called when any change is made to volume slider
+
 func _on_master_volume_value_changed(value):
 
 	AudioServer.set_bus_volume_db(0,value)
@@ -45,34 +40,31 @@ func _on_master_volume_value_changed(value):
 
 
 
-#Called when back button in pause menu is pressed
+
+
 func _on_back_button_pressed():
 	get_tree().paused = false
 	$GUI/PauseButton.show()
 	$"GUI/Pause Menu".hide()
 	$GUI/PauseMenuBackground.hide()
 
-#Called when Menu button is pressed in pause menu
+
 func _on_menu_button_pressed():
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://MainMenu/MainMenu.tscn")
-	
-#Called when restart button is pressed in pause menu
+
+
 func _on_restart_button_pressed():
 	get_tree().reload_current_scene()
 	get_tree().paused = false
 	_change_level(Globalvars.getLevel())
 
 
-
-#Adds one to coin counter
 func _add_coin():
 	coinCounter += 1
-
-#returns counterCounter float
+	
 func _get_coin_counter():
 	return coinCounter
 
-#sets coinCounter to parameter
 func _set_counter(set_to):
 	coinCounter = set_to
