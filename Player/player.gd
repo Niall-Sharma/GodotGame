98c4a0461525f0ba1f271746af51d8e870a0ad8e
PerpetualObligation <143425661+PerpetualObligation@users.noreply.github.com>
2023-10-24 12:54:03 -0500

    makes the attack switch directions

--- Player/player.gd
+++ Player/player.gd
@@ -42,7 +42,13 @@ func _physics_process(_delta):
 			
 		if direction > 0:
 			$Sprite2D.flip_h = false
-			
+	
+	if direction>0 and $AttackArea.position.x < $PlayerShape.position.x:
+		$AttackArea.position.x+=50
+		print("<0")
+	if direction<0 and $AttackArea.position.x > $PlayerShape.position.x:
+		$AttackArea.position.x-=50
+		print(">0")
 
 #Player loses health when called
 func take_damage(damage):
@@ -110,3 +116,4 @@ func highJump():
 
 func isHealthMax():
 	return health < 100
+
