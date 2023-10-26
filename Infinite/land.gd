cf2709f90bd62d8594897720f4829bf6e1eff611
AsterSailor <artanddna@gmail.com>
2023-10-26 12:04:29 -0500

    counter for infinite!!

--- Infinite/land.gd
+++ Infinite/land.gd
@@ -1,11 +1,11 @@
 extends StaticBody2D
 
-var count = 0
+
 
 func _on_land_area_body_entered(body):
 	if body.name == "Player":
-		if count == 0:
-			var area = self.get_child(0)
+		var area = self.get_child(0)
+		print(area.name)
+		if area.name == "land_area":
 			remove_child(area)
-			count == 1
-		$/root/Master._add_coin()
+			$/root/Master._add_coin()
