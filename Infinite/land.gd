2fe7cbbc4b96812467a6985e73d6dfbf08f8e6bb
AsterSailor <artanddna@gmail.com>
2023-10-26 12:39:10 -0500

    tiles

--- Infinite/land.gd
+++ Infinite/land.gd
@@ -5,7 +5,7 @@ extends StaticBody2D
 func _on_land_area_body_entered(body):
 	if body.name == "Player":
 		var area = self.get_child(0)
-		print(area.name)
+
 		if area.name == "land_area":
 			remove_child(area)
 			$/root/Master._add_coin()
