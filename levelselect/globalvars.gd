62f2176a33d6d3d2fa0dbe0ee5deb7f984cdb9b6
AsterSailor <artanddna@gmail.com>
2023-10-27 13:02:37 -0500

    ok

--- levelselect/globalvars.gd
+++ levelselect/globalvars.gd
@@ -1,7 +1,11 @@
 extends Node2D
 
 var lvl_num
+<<<<<<< HEAD
 var lc = [true,true,true,false,false,false,false,false,false,false,true] 
+=======
+var lc = [true,true,false,false,false,false,false,false,false,false,true] 
+>>>>>>> 75cb0dd0baaeb4efc58e8ac7fb3d20f61f3f21fc
 	
 
 
