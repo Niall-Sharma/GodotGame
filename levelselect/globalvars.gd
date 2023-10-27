89f1e22de1c782fca140df4646a7f2c2afc77d02
Spork06 <spenserpraska@gmail.com>
2023-10-27 13:33:49 -0500

    Update globalvars.gd

--- levelselect/globalvars.gd
+++ levelselect/globalvars.gd
@@ -1,11 +1,7 @@
 extends Node2D
 
 var lvl_num
-<<<<<<< HEAD
-var lc = [true,true,true,false,false,false,false,false,false,false,true] 
-=======
 var lc = [true,true,false,false,false,false,false,false,false,false,true] 
->>>>>>> 75cb0dd0baaeb4efc58e8ac7fb3d20f61f3f21fc
 	
 
 
