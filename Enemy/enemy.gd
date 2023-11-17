4f0c5d536c6505a04717083cfd742d728644d94f
Spork06 <spenserpraska@gmail.com>
2023-11-17 10:39:50 -0600

    e

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -18,20 +18,21 @@ var coin_scene = preload("res://Coin/coin.tscn")
 var raycast_right
 var raycast_left
 var is_locked : bool = false
+var is_locked2 : bool = false
 var last_valid_position : Vector2
 var falling = false
 var player
 var checkright : bool = false
 var checkleft : bool = false
 var quickcheck : bool = false
-
+var quickcheck2: bool = false
 
 
 func  _ready():
 	animationTree.active = true
-	raycast_right = $RayCast2D
+	raycast_right = $RayCastRight
 	raycast_right.enabled = true
-	raycast_left = $RayCast2D2
+	raycast_left = $RayCastLeft
 	raycast_left.enabled = true
 	
 	last_valid_position = position
@@ -45,69 +46,118 @@ func jump():
 
 
 
+#func _physics_process(delta):
+#	# setting variables
+#	checkleft = false
+#	checkright = false
+#	var left_ray_hit = raycast_left.is_colliding()
+#	var right_ray_hit = raycast_right.is_colliding()
+#	var direction_to_player = (PLAYER.global_position - global_position).normalized()
+#	var enemy_forward_direction = -transform.x
+#
+#	# Check if both raycasts are not hitting the tilemap
+#	if not right_ray_hit:
+#		is_locked = true
+#		quickcheck = true
+#	if not left_ray_hit:
+#		print("stuck")
+#		is_locked = true
+#		print("stucks", is_locked)
+#		quickcheck2 = true
+#		print("y")
+#
+#	#If player is to the left of enemy when its stuck. Get unstuck
+#	if direction_to_player.dot(enemy_forward_direction) > 0 and quickcheck == true:
+#		print("Player is to the left of me!")
+#		checkleft = true
+#	if direction_to_player.dot(enemy_forward_direction) < 0 and quickcheck2 == true:
+#		print("Player is to the right of me!")
+#		checkright = true
+#
+#
+#	direction = (global_position - PLAYER.global_position).normalized()
+#
+#	#Checking if it can get unstuck to the left
+#	if checkleft == true:
+#		is_locked = false
+#	#Checking if it can get unstuck to the right
+#	if checkright == true:
+#		is_locked = false
+#
+#
+#	#Flipping sprite
+#	if direction.x > 0:
+#			sprite.flip_h = true
+#	else:
+#			sprite.flip_h = false
+#	if not is_locked:
+#		# Add the gravity.
+#		if not is_on_floor():
+#			velocity.y += gravity * delta
+#			falling == true
+#		#Flip sprite depending on which way the enemy is moving
+#		if direction.x > 0:
+#			sprite.flip_h = true
+#		else:
+#			sprite.flip_h = false
+#		#Only move if state boolean canMove returns true	
+#		if STATE_MACHINE.checkCanMove():
+#			velocity.x = direction.x*SPEED
+#	#			var yDiff = PLAYER.global_position.y - global_position.y
+#	#			if (yDiff < -1 or yDiff > 1) and is_on_floor() and canJump:
+#	#				jump()
+#	#				jumpTimer.start()
+#	#				canJump = false
+#		else:
+#			velocity.x = 0
+#
+#		move_and_slide()
+
+
 func _physics_process(delta):
-	# setting variables
-	checkleft = false
-	checkright = false
 	var left_ray_hit = raycast_left.is_colliding()
 	var right_ray_hit = raycast_right.is_colliding()
 	var direction_to_player = (PLAYER.global_position - global_position).normalized()
 	var enemy_forward_direction = -transform.x
-	
-	# Check if both raycasts are not hitting the tilemap
-	if not right_ray_hit:
+	# Debug prints for troubleshooting
+	print("Left Ray Hit:", left_ray_hit)
+	print("Right Ray Hit:", right_ray_hit)
+
+	if left_ray_hit and right_ray_hit:
+		is_locked = false
+	else:
 		is_locked = true
-		quickcheck = true
-	else:
-		is_locked = false
-#
-#	if not left_ray_hit:
-#		is_locked = true
-#		print("left is locking randomly")
-#	else:
-#		is_locked = false
 
 
-	#Going back to following player once is gets locked
-	if direction_to_player.dot(enemy_forward_direction) < 0 and quickcheck == true:
-		print("Player is to the right of me!")
-		checkright = true
-	else:
-		print("Player is to the left of me!")
-		checkleft = true
+	if direction_to_player.dot(enemy_forward_direction) > 0 and not right_ray_hit:
+			print("Player is to the left of me!")
+			is_locked = false
+	if direction_to_player.dot(enemy_forward_direction) < 0 and not left_ray_hit:
+			print("Player is to the left of me!")
+			is_locked = false
+	# Check if both raycasts are hitting the tilemap
+	if is_locked == false:
+		
+		# Set the direction for the enemy based on the player's position
+		direction = (global_position - PLAYER.global_position).normalized()
 
-	direction = (global_position - PLAYER.global_position).normalized()
-	
-	if checkleft == true:
-		is_locked = false
-	
-	#Flipping sprite
-	if direction.x > 0:
-			sprite.flip_h = true
-	else:
-			sprite.flip_h = false
-	if not is_locked:
-		# Add the gravity.
-		if not is_on_floor():
-			velocity.y += gravity * delta
-			falling == true
-		#Flip sprite depending on which way the enemy is moving
+		# Flipping sprite
 		if direction.x > 0:
 			sprite.flip_h = true
 		else:
 			sprite.flip_h = false
-		#Only move if state boolean canMove returns true	
+
+		# Only move if the state boolean canMove returns true
 		if STATE_MACHINE.checkCanMove():
-			velocity.x = direction.x*SPEED
-	#			var yDiff = PLAYER.global_position.y - global_position.y
-	#			if (yDiff < -1 or yDiff > 1) and is_on_floor() and canJump:
-	#				jump()
-	#				jumpTimer.start()
-	#				canJump = false
+			velocity.x = direction.x * SPEED
 		else:
 			velocity.x = 0
-		
+
 		move_and_slide()
+
+
+
+
 
 #Taking damage function
 func takeDamage(damage):
