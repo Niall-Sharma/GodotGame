76044114a8880d73e0da72812be8a0f81a2aafc5
AsterSailor <artanddna@gmail.com>
2023-11-21 13:02:45 -0600

    ghj

---
+++ penguin/penguin.gd
@@ -0,0 +1,37 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+var health = 100
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+func throw():
+	await get_tree().create_timer(3).timeout
+	
+	
+func takeDamage(damage):
+	health -= damage
+	if health<=0:
+		get_tree().queue_delete(self)
