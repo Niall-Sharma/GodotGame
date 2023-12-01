ad6e4b90c855caf4ee8991b18e2a5a94f1fe3113
AsterSailor <artanddna@gmail.com>
2023-12-01 13:43:47 -0600

    SNOW

--- snowball/snowball.gd
+++ snowball/snowball.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
 @onready var PLAYER : CharacterBody2D = $"../Player"
 
-var px = PLAYER.position.x
-var py = PLAYER.position.y
-var x = self.position.x
-var y = self.position.y
+@onready var px = PLAYER.position.x
+@onready var py = PLAYER.position.y
+@onready var x = self.position.x
+@onready var y = self.position.y
 @onready var vec = Vector2(py-y,px-x)
+@onready var speed = 200
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	null
@@ -16,12 +18,18 @@ func _process(delta):
 	
 	if abs(PLAYER.position.x - self.position.x) < 500:
 			await get_tree().create_timer(3).timeout
-			print("Help me")
-
+			var target = PLAYER.position
+			velocity = position.direction_to(target) * speed
+			# look_at(target)
+			if position.distance_to(target) > 10:
+				move_and_slide()
+func _physics_process(delta):
+	pass
 
 
 func _on_area_2d_body_entered(body):
 	if body.name == "Player":
 		get_tree().queue_delete(self)
 		body.take_damage(10)
-		
+	else:
+		get_tree().queue_delete(self)
