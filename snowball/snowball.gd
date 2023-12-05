0346878e39d25630495e400a2c9bd4989c76d0e5
AsterSailor <artanddna@gmail.com>
2023-12-05 13:11:18 -0600

    snowballs

--- snowball/snowball.gd
+++ snowball/snowball.gd
@@ -6,30 +6,50 @@ extends CharacterBody2D
 @onready var x = self.position.x
 @onready var y = self.position.y
 @onready var vec = Vector2(py-y,px-x)
-@onready var speed = 200
+@onready var speed = 230
+@onready var moving = false
+@onready var target
+@onready var snoa = self.duplicate()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	null
+	pass
 
+func dupe():
+	
+	snoa.position.x = x
+	snoa.position.y = y
+	$"..".add_child(snoa)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
 	if abs(PLAYER.position.x - self.position.x) < 500:
-			await get_tree().create_timer(3).timeout
-			var target = PLAYER.position
-			velocity = position.direction_to(target) * speed
+		if (moving == false):
+			await get_tree().create_timer(2).timeout
+			moving = true
+		target = PLAYER.position
+		velocity = position.direction_to(target) * speed
+		dupe()
 			# look_at(target)
-			if position.distance_to(target) > 10:
-				move_and_slide()
+		if position.distance_to(target) > 10:
+			move_and_slide()
+			
+				
 func _physics_process(delta):
 	pass
 
-
 func _on_area_2d_body_entered(body):
 	if body.name == "Player":
+		var neg = 0
+		if (self.position.x < body.position.x):
+			neg = 1
+			
+		if (self.position.x > body.position.x) :
+			neg = -1
 		get_tree().queue_delete(self)
-		body.take_damage(10)
+		
+		body.take_damage(10, Vector2(neg*1000,0), false)
+		
 	else:
 		get_tree().queue_delete(self)
