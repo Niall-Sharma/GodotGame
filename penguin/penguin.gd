54884c63e51dd627571b95a76dbebec930080462
AsterSailor <artanddna@gmail.com>
2023-12-12 12:25:33 -0600

    moew

--- penguin/penguin.gd
+++ penguin/penguin.gd
@@ -14,7 +14,8 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		
+	$AnimatedSprite2D.show()
+	
 	
 
 	"if PLAYER.position.x > self.position.x:
