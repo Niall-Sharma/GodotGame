e74def17fba5300a00b81ec34a29c346545f2463
Spork06 <spenserpraska@gmail.com>
2024-01-03 10:11:07 -0600

    lvl6 done

--- Level6/lvl6lock.gd
+++ Level6/lvl6lock.gd
@@ -8,8 +8,10 @@ var lvl6hehe = preload("res://lvl6hehe/lvl6he.tscn")
 
 func _on_body_entered(body):
 	if body.name == "Player":
-		await get_tree().create_timer(1.5).timeout
+		await get_tree().create_timer(0.5).timeout
 		var newtile = lvl6hehe.instantiate()
 		add_child(newtile)
 		print("done for")
+		await get_tree().create_timer(5).timeout
+		remove_child(newtile)
 
